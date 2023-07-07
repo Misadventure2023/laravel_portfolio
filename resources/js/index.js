8b27d152f8316e75b6467f0225a9ec97d39aad8d
ryoku25 <40714054+ryoku25@users.noreply.github.com>
2023-07-07 18:53:38 +0800

    Skill Carousel

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
 document.read
 const burger = document.querySelector("#burger");
@@ -64,3 +66,83 @@ $('#to-top').click(function (e) {
     e.preventDefault();
     
 });
+
+// $('.logos-slide').mousemove(function (e) { 
+//     // values: e.clientX, e.clientY, e.pageX, e.pageY
+//     $(this).scrollLeft(e.pageX);
+// });
+/// Skill Icon Carousel
+
+const carousel = document.querySelector('.logos');
+const arrowBtn = document.querySelectorAll(".logo-wrapper svg");
+const firstIcon = carousel.querySelector(".logos-slide img").offsetWidth;
+const carouselChildren = [... carousel.children];
+const wrapper = document.querySelector('.logo-wrapper');
+let isDragging = false , startX,  startScrollLeft, timeOutId;
+
+// Get number of icons that can fit  in the carousel
+let iconPreview = Math.round(carousel.offsetWidth / firstIcon);
+
+carouselChildren.slice(-iconPreview).reverse().forEach(icon =>{
+    carousel.insertAdjacentHTML("afterbegin", icon.outerHTML);
+});
+
+carouselChildren.slice(0, iconPreview).reverse().forEach(icon =>{
+    carousel.insertAdjacentHTML("beforeend", icon.outerHTML);
+});
+
+const dragStart = (e)=> {
+    isDragging = true;
+    carousel.classList.add ("dragging");
+
+    startX =e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+
+}
+const dragging = (e)=> {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+    const dragStop = ()=>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+// add event listener to arrow buttons
+arrowBtn.forEach(btn => {
+    btn.addEventListener("click", () =>{
+        
+        carousel.scrollLeft += btn.id === "left" ? -firstIcon : firstIcon;
+    })
+});
+
+const infiniteScroll = ()=> {
+    if(carousel.scrollLeft === 0){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2* carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }else if(Math.ceil(carousel.scrollLeft) == carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition");
+       carousel.scrollLeft = carousel.offsetWidth;
+       carousel.classList.remove("no-transition");
+
+    }
+    clearTimeout(timeOutId);
+    if(!wrapper.matches(':hover')) autoPlay();
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 200) return; // return if with is less than 800
+    timeOutId = setTimeout(()=> carousel.scrollLeft += firstIcon, 2500);
+}
+
+autoPlay();
+carousel.addEventListener("mousemove",dragging);
+carousel.addEventListener("mousedown",dragStart);
+document.addEventListener("mouseup",dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", ()=> clearTimeout(timeOutId));
+wrapper.addEventListener("mouseleave",autoPlay);
+
+
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio</title>
     <link rel="icon" type="image/x-icon" href="{{URL::asset('assets/img/Logowhitebg.jpg')}}">
+
     @vite('resources/css/app.css')
+
 </head>
 <body class="bg-gray-100 font-sans  min-h-screen max-w-screen-2xl mx-auto flex justify-between flex-col ">
